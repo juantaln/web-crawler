c0772c7ec7b35bbca28985415c847454f96bb999
Juan David Taborda Londoño <jtaborda@sura.com.co>
2025-07-21 16:43:19 -0500

    Fix: Añade @ComponentScan explícito para asegurar la carga de configuración

--- src/main/java/com/portafolio/web_crawler/WebCrawlerApplication.java
+++ src/main/java/com/portafolio/web_crawler/WebCrawlerApplication.java
@@ -2,8 +2,10 @@ package com.portafolio.web_crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = "com.portafolio.web_crawler")
 @SpringBootApplication
+
 public class WebCrawlerApplication {
 
 	public static void main(String[] args) {
