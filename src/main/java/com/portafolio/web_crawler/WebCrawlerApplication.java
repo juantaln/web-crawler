a1e5d9500dd9558faaab0b94978a63927a68a2a8
Juan David Taborda Londoño <jtaborda@sura.com.co>
2025-07-21 18:23:06 -0500

    Fix: modificacion clase principal para eliminar seguridad

--- src/main/java/com/portafolio/web_crawler/WebCrawlerApplication.java
+++ src/main/java/com/portafolio/web_crawler/WebCrawlerApplication.java
@@ -2,14 +2,13 @@ package com.portafolio.web_crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-@ComponentScan(basePackages = "com.portafolio.web_crawler")
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) 
 public class WebCrawlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebCrawlerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebCrawlerApplication.class, args);
+    }
 
 }
